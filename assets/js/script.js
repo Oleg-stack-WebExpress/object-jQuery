5465bfafc35ce04fbda4e3863b17e65a2fbc09de
Your Name <you@example.com>
2025-05-02 01:36:23 +0700

    Работа с заданиями 2 и 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@
 
 var arrProducts = [
     { name: 'product30', price: 30, count: 5 },
-    { name: 'product120', price: 120, count: 3 },
+    { name: 'product140', price: 140, count: 3 },
     { name: 'product50', price: 50, count: 6 },
-    { name: 'product140', price: 140, count: 2 }
+    { name: 'product120', price: 120, count: 2 }
 ];
 
 for (var i = 0; i < arrProducts.length; i++) {
@@ -26,28 +26,19 @@ console.log(arrProductsAbove100);
 
 // Задание 2
 
-var maxPriceProduct = 1;
+var getProductWithMaxPrice = function () {
+    if (arrProducts.length === 0) return null; // Проверка на пустой массив
 
-var getMaxPrice = function () {
-    for (var i = 0; i < arrProducts.length; i++) {
-        if (arrProducts[i].price > maxPriceProduct) {
-            maxPriceProduct = arrProducts[i].price;
+    var productWithMaxPrice = arrProducts[0];
+
+    for (var i = 1; i < arrProducts.length; i++) {
+        if (arrProducts[i].price > productWithMaxPrice.price) {
+            productWithMaxPrice = arrProducts[i]; // Просто переприсваиваем товар
         }
     }
-    return {
-        ProductWithMaxPrice: function () {
-            for (var i = 0; i < arrProducts.length; i++) {
-                if (arrProducts[i].price == maxPriceProduct) {
-                    return (arrProducts[i]);
-                }
-            }
-        }
-    }
-}
 
-const myMaxPrice = getMaxPrice();
-
-console.log(myMaxPrice.ProductWithMaxPrice);
+    return productWithMaxPrice; // Вернётся один товар с максимальной ценой
+};
 
 
 // Задание 3
